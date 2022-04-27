24a205b2e4d80430772d6f96791f6274dc8fa8d1
Manu Ardeiro <102896906+ManuArdeiro@users.noreply.github.com>
2022-04-27 10:40:22 +0200

    Update libftprintf.h

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -27,25 +27,28 @@ typedef struct s_options
 	int	plus;
 	int	space;
 	int	pad;
-}				t_options;
+}		t_options;
 
-int			ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
 
-int			ft_printf_write_char(char c, int fd);
+int		ft_printf_write_char(char c, int fd);
 t_options	*ft_printf_flags_init(t_options *flags);
-int			ft_printf_strlen(char *str);
-int			ft_printf_itoa_len(int j);
+int		ft_printf_strlen(char *str);
+int		ft_printf_itoa_len(int j);
 void		ft_print_write_itoa(int j, int *sol, t_options *flags);
 
 void		ft_var_print_main(char c, va_list args, int *sol, t_options *flags);
 void		ft_var_print_str_left(char *str, int *sol, t_options *flags);
 void		ft_var_print_str_right(char *str, int *sol, t_options *flags);
-void		ft_var_print_pointer(void *ptr, int *sol, t_options *flags);
+void		ft_var_print_hex_left(char c, unsigned long int hex, int *sol,
+			t_options *flags);
+void		ft_var_print_hex_right(char c, unsigned long int hex, int *sol,
+			t_options *flags);
 
 void		ft_var_print_nbr_left(int j, int *sol, t_options *flags);
 void		ft_var_print_nbr_right(int j, int *sol, t_options *flags);
 void		ft_var_print_unsigned_left(unsigned int j, int *sol, t_options *flags);
-void		ft_var_print_hex_right(char c, unsigned long int hex, int *sol,
-				t_options *flags);
+void		ft_var_print_pointer(void *ptr, int *sol, t_options *flags);
+
 
 #endif
