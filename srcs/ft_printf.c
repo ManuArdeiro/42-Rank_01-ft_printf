2f7230d9584a629f1d354b621e1d7d9ed7b79344
Manu Ardeiro <102896906+ManuArdeiro@users.noreply.github.com>
2022-04-28 12:46:11 +0200

    Update ft_printf.c

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -90,29 +90,3 @@ int	ft_printf(char const *text, ...)
 	va_end(args);
 	return (*sol);
 }
-
-/*int			main(void)
-{
-	char			c = 'A';
-	char			*string = "En un lugar de La Mancha...";
-	void			*puntero = &c;
-	int				entero = -10;
-	int				intero = 10;
-	float			numero = 3.141592;
-	int				octal = 012345;
-	unsigned int	sin_signo = 12345;
-	int 			hexadecimal = 0xabc23;
-	int				HEXADECIMAL = 0XABC23;
-		
-	ft_printf("%%d escribre un número decimal: %d\n", entero);
-	ft_printf("%%c escribe un caracter: %c\n", c);
-	ft_printf("%%s escribe un string: %s\n", string);
-	ft_printf("%%p escribe un puntero: %p\n", puntero);
-	ft_printf("%%i escribe numeros enteros: %i\n", intero);
-	ft_printf("%%o escribe numeros octales: %o\n", octal);
-	ft_printf("%%u escribe un número sin signo: %u\n", sin_signo);
-	ft_printf("%%x escribe hexadecimales en minusculas: %x\n", hexadecimal);
-	ft_printf("%%X escribe hexadecimales en mayusculas: %X\n", HEXADECIMAL);
-	ft_printf("%%f escribe un float: %f\n", numero);
-	return (0);
-}*/
