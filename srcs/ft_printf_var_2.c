9ba3eed89ee311bcb9ab9ba1219f35a681c380f2
Manu Ardeiro <102896906+ManuArdeiro@users.noreply.github.com>
2022-05-05 12:30:14 +0200

    Update ft_printf_var_2.c

--- srcs/ft_printf_var_2.c
+++ srcs/ft_printf_var_2.c
@@ -84,14 +84,14 @@ void	ft_var_print_unsigned_left(unsigned int j, int *sol, t_options *flags)
 	}
 	if (flags->plus == 0 && flags->space == 0)
 		flags->precision = flags->precision + 1;
-	k = ft_printf_itoa_len(j);
+	k = ft_printf_itoa_unsigned_len(j);
 	while (k++ < flags->precision)
 		*sol = *sol + ft_printf_write_char('0', 1);
 	if (flags->plus == 1 && j > 0)
 		*sol = *sol + ft_printf_write_char('+', 1);
 	else if (flags->space == 1 && j > 0)
 		*sol = *sol + ft_printf_write_char(' ', 1);
-	ft_printf_write_itoa(j, sol);
+	ft_printf_write_itoa_unsigned(j, sol);
 	while (k++ < flags->width && flags->zero == 0)
 		*sol = *sol + ft_printf_write_char(' ', 1);
 	while (k++ < flags->width && flags->zero == 1)
@@ -116,14 +116,14 @@ void	ft_var_print_unsigned_right(unsigned int j, int *sol, t_options *flags)
 		*sol = *sol + ft_printf_write_char(' ', 1);
 	while (k++ < flags->width && flags->zero == 1)
 		*sol = *sol + ft_printf_write_char('0', 1);
-	k = ft_printf_itoa_len(j);
+	k = ft_printf_itoa_unsigned_len(j);
 	while (k++ < flags->precision)
 		*sol = *sol + ft_printf_write_char('0', 1);
 	if (flags->plus == 1 && j > 0)
 		*sol = *sol + ft_printf_write_char('+', 1);
 	else if (flags->space == 1 && j > 0)
 		*sol = *sol + ft_printf_write_char(' ', 1);
-	ft_printf_write_itoa(j, sol);
+	ft_printf_write_itoa_unsigned(j, sol);
 }
 
 void	ft_var_print_pointer(void *ptr, int *sol, t_options *flags)
