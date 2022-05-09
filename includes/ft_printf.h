1dfba756fdbb781984fb1aac71d336a13484c416
Manu Ardeiro <102896906+ManuArdeiro@users.noreply.github.com>
2022-05-09 09:15:26 +0200

    Update ft_printf.h

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -33,47 +33,71 @@ typedef struct s_options
 // ft_printf.c
 int			ft_printf(const char *format, ...);
 
-// ft_printf_utils_1.c
-int			ft_printf_write_char(char c, int fd);
-t_options	*ft_printf_flags_init(t_options *flags);
-int			ft_printf_strlen(char *str);
-int			ft_printf_itoa_len(int j);
-void		ft_printf_write_itoa(int j, int *sol);
+// ft_printf_manage_flags.c
 
-// ft_printf_utils_2.c
-int			ft_printf_itoa_len_hex(unsigned long int hex);
-void		ft_printf_write_itoa_hex(char c, unsigned long int hex, int *sol);
-void		ft_printf_write_hex_l(unsigned long int hex_l, int *sol,
-				char *digits);
-void		ft_printf_write_hex_u(unsigned long int hex_u, int *sol,
-				char *digits);
+int			ft_var_set_length(int length, char c, int *i);
+void			ft_var_set_flags(t_options *flags, char c, int *i);
+void			ft_var_read_flags(char const *text, int *i, t_options *flags);
 
-//ft_printf_utils_3.c
-int 		ft_sign_change(int j, int *sol);
-int			ft_printf_itoa_unsigned_len(unsigned int j);
-void		ft_printf_write_itoa_unsigned(unsigned int j, int *sol);
+// ft_printf_select_var.c
 
-// ft_printf_var_1.c
 void		ft_var_print_main(char c, va_list args, int *sol, t_options *flags);
-void		ft_var_print_str_left(char *str, int *sol, t_options *flags);
-void		ft_var_print_str_right(char *str, int *sol, t_options *flags);
-void		ft_var_print_hex_left(char c, unsigned int hex, int *sol,
-				t_options *flags);
-void		ft_var_print_hex_right(char c, unsigned int hex, int *sol,
-				t_options *flags);
+int		ft_printf_write_char(char c, int fd);
 
-// ft_printf_var_2.c
+// ft_printf_numbers.c
+
 void		ft_var_print_nbr_left(int j, int *sol, t_options *flags);
 void		ft_var_print_nbr_right(int j, int *sol, t_options *flags);
 void		ft_var_print_unsigned_left(unsigned int j, int *sol,
 				t_options *flags);
 void		ft_var_print_unsigned_right(unsigned int j, int *sol,
 				t_options *flags);
+
+// ft_printf_chars.c
+
+void	ft_var_print_chr_right(int c, int *sol, t_options *flags)
+void	ft_var_print_chr_left(int c, int *sol, t_options *flags)
+
+//ft_printf_strings.c
+	
+void		ft_var_print_str_left(char *str, int *sol, t_options *flags);
+void		ft_var_print_str_right(char *str, int *sol, t_options *flags);
+
+// ft_printf_pointer.c
+
 void		ft_var_print_pointer(void *ptr, int *sol, t_options *flags);
 
-// ft_printf_var_3.c
-void		ft_var_print_chr_left(int c, int *sol, t_options *flags);
-void		ft_var_print_chr_right(int c, int *sol, t_options *flags);
+// ft_printf_hex.c
+
+void		ft_var_print_hex_left(char c, unsigned int hex, int *sol,
+				t_options *flags);
+void		ft_var_print_hex_right(char c, unsigned int hex, int *sol,
+				t_options *flags);
+
+// ft_printf_utils_len.c
+int			ft_printf_strlen(char *str);
+int			ft_printf_strlen(char *str);
+int			ft_printf_itoa_unsigned_len(unsigned int j);
+int			ft_printf_itoa_len(int j);
+
+// ft_printf_utils_write_hex.c
+void		ft_printf_write_itoa_hex(char c, unsigned long int hex, int *sol);
+void		ft_printf_write_hex_l(unsigned long int hex_l, int *sol,
+				char *digits);
+void		ft_printf_write_hex_u(unsigned long int hex_u, int *sol,
+				char *digits);
+
+// ft_printf_utils_write_itoa.c
+
+void		ft_printf_write_itoa(int j, int *sol);
+void		ft_printf_write_itoa_unsigned(unsigned int j, int *sol);
+
+// ft_printf_utils_flags_init.c
+t_options	*ft_printf_flags_init(t_options *flags);
+
+
+//ft_printf_utils_sign_change.c
+int 		ft_sign_change(int j, int *sol);
 
 
 #endif
