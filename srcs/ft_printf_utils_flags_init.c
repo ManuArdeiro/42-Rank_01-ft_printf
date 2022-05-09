5ed4b131bd870577496623792eb57e2a0cd30d59
Manu Ardeiro <102896906+ManuArdeiro@users.noreply.github.com>
2022-05-09 09:09:06 +0200

    Update and rename ft_printf_utils_1.c to ft_printf_utils_flags_init.c

--- srcs/ft_printf_utils_flags_init.c
+++ srcs/ft_printf_utils_flags_init.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_utils_1.c                                :+:      :+:    :+:   */
+/*   ft_printf_utils_flags_init.c                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jolopez- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -22,32 +22,4 @@ t_options	*ft_printf_flags_init(t_options *flags)
 	flags->width = 0;
 	flags->precision = -1;
 	return (flags);
-}
-
-void	ft_printf_write_itoa(int j, int *sol)
-{
-	int		len;
-	char	*str;
-
-	if (j == 0)
-		*sol = *sol + ft_printf_write_char('0', 1);
-	if (j < 0)
-		j = ft_sign_change(j, sol);
-	len = ft_printf_itoa_len(j);
-	str = (char *)malloc(sizeof(char) * len);
-	if (!str)
-	{
-		free (str);
-		*sol = -1;
-		return ;
-	}
-	str[len] = '\0';
-	while (j > 0)
-	{
-		str[--len] = '0' + j % 10;
-		j = (j / 10);
-	}
-	while (str[len])
-		*sol = *sol + ft_printf_write_char(str[len++], 1);
-	free(str);
 }
