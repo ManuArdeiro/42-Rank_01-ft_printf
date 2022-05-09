bca04d270f8127512873cd00279db3a7d652d049
Manu Ardeiro <102896906+ManuArdeiro@users.noreply.github.com>
2022-05-09 09:13:03 +0200

    Create ft_printf_utils_write_itoa.c

---
+++ srcs/ft_printf_utils_write_itoa.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils_write_itoa.c                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jolopez- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 17:21:47 by jolopez-          #+#    #+#             */
+/*   Updated: 2022/05/08 16:24:59 by jolopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+void	ft_printf_write_itoa(int j, int *sol)
+{
+	int		len;
+	char	*str;
+
+	if (j == 0)
+		*sol = *sol + ft_printf_write_char('0', 1);
+	if (j < 0)
+		j = ft_sign_change(j, sol);
+	len = ft_printf_itoa_len(j);
+	str = (char *)malloc(sizeof(char) * len);
+	if (!str)
+	{
+		free (str);
+		*sol = -1;
+		return ;
+	}
+	str[len] = '\0';
+	while (j > 0)
+	{
+		str[--len] = '0' + j % 10;
+		j = (j / 10);
+	}
+	while (str[len])
+		*sol = *sol + ft_printf_write_char(str[len++], 1);
+	free(str);
+}
+
+void	ft_printf_write_itoa_unsigned(unsigned int j, int *sol)
+{
+	int		len;
+	char	*str;
+
+	if (j == 0)
+		*sol = *sol + ft_printf_write_char('0', 1);
+	len = ft_printf_itoa_unsigned_len(j);
+	str = (char *)malloc(sizeof(char) * len);
+	if (!str)
+	{
+		free (str);
+		*sol = -1;
+		return ;
+	}
+	str[len] = '\0';
+	while (j > 0)
+	{
+		str[--len] = '0' + j % 10;
+		j = (j / 10);
+	}
+	while (str[len])
+		*sol = *sol + ft_printf_write_char(str[len++], 1);
+	free(str);
+}
