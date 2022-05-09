7c8cd85f9d2161cd1b55802fb479c76f35136d08
Manu Ardeiro <102896906+ManuArdeiro@users.noreply.github.com>
2022-05-09 08:31:12 +0200

    Update ft_printf_bonus.c

--- srcs/ft_printf_bonus.c
+++ srcs/ft_printf_bonus.c
@@ -13,43 +13,6 @@
 
 #include "../includes/ft_printf.h"
 
-static int	ft_var_set_length(int length, char c, int *i)
-{	
-	length = (length * 10) + (c - '0');
-	*i = *i + 1;
-	return (length);
-}
-
-static void	ft_var_set_flags(t_options *flags, char c, int *i)
-{
-	if (c == '-')
-		flags->minus = 1;
-	if (c == '+')
-		flags->plus = 1;
-	if (c == ' ')
-		flags->space = 1;
-	if (c == '0')
-		flags->zero = 1;
-	if (c == '#')
-		flags->pad = 1;
-	*i = *i +1;
-}
-
-static void	ft_var_read_flags(char const *text, int *i, t_options *flags)
-{
-	while (text[*i] == '-' || text[*i] == '0' || text[*i] == '#'
-		|| text[*i] == ' ' || text[*i] == '+')
-		ft_var_set_flags(flags, text[*i], i);
-	while ((text[*i] >= '0' && text[*i] <= '9'))
-		flags->width = ft_var_set_length(flags->width, text[*i], i);
-	if (text[*i] == '.')
-	{
-		*i = *i + 1;
-		while (text[*i] >= '0' && text[*i] <= '9')
-			flags->precision = ft_var_set_length(flags->precision, text[*i], i);
-	}
-}
-
 static void	ft_var_print(char const *text, va_list args, int *i, int *sol)
 {
 	t_options	*flags;
